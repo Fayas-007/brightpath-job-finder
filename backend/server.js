24a230b4169c27343009e5461bb2c672caa85ff7
FAYAS <fayasshibly123@gmail.com>
2026-07-24 18:58:22 +0530

    Add Vercel backend deployment config

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV !== "test") {
+if (process.env.NODE_ENV !== "test" && !process.env.VERCEL) {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
 
